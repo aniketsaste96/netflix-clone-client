d0c0511d53d979fc100743032b6f29b4df4c8069
Aniket <aniketsaste96@gmail.com>
2022-04-04 17:07:56 +0530

    icons done

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./list.scss";
 import {
   ArrowBackIosOutlined,
@@ -6,28 +6,48 @@ import {
 } from "@material-ui/icons";
 import ListItem from "../listItem/ListItem";
 const List = () => {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+  const listRef = useRef();
+  //its same like query selctor or select element by Id
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      setSlideNumber(slideNumber - 1);
+    }
+    if (direction === "right" && slideNumber < 5) {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      setSlideNumber(slideNumber + 1);
+    }
+  };
+
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left" />
-        <div className="container">
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
         </div>
-        <ArrowForwardIosOutlined className="sliderArrow right" />
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./listitem.scss";
-const ListItem = () => {
-  return <div className="listItem">items</div>;
+import {
+  PlayArrow,
+  ThumbUpAltOutlined,
+  Add,
+  ThumbDownAltOutlined,
+} from "@material-ui/icons";
+const ListItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer = "https://vimeo.com/253484691";
+  return (
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src="https://wallpapercave.com/wp/wVMsPCF.jpg" alt="" />
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop controls />
+
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownAltOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In,
+              consequatur earum incidunt nam quam odit asperiores.
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ListItem;
