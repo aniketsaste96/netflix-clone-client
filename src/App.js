a7b8dd3e594ce6a3955c99c084274b73395dd0ca
Aniket <aniketsaste96@gmail.com>
2022-04-05 11:31:02 +0530

    register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.scss';
 import Home from './pages/home/Home';
+import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
 
 function App() {
   return (
     <div className="App">
-      <Watch />
+      <Register />
     </div>
   );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./register.scss";
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+    setPassword(passwordRef.current.value);
+  };
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value);
+  };
   return (
     <div className="register">
       <div className="top">
-        <img
-          className="logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-          alt=""
-        />
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited Movies,TV shows and many more</h1>
+        <h2>Wtach anywhere,Cancel anytime</h2>
+        <p>
+          Ready to waych Enter Your mail to create or restart your membership
+        </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="Email Address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
