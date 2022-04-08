16542c4c3a494167a23447cd64a9e80389b70902
Aniket <aniketsaste96@gmail.com>
2022-04-08 20:34:40 +0530

    setting router

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
-
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/movies">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
